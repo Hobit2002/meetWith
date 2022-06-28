97d79f66e7ba219038ccad665840346dbb0d2d3d
David Nadrchal <davidnadrchalintj@gmail.com>
2022-06-29 00:11:36 +0200

    design for mobile phones

--- meetWith/settings.py
+++ meetWith/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':'Zpeneni',
         'USER':'Zpeneni',
-        'PASSWORD':'ritAyoPI',
+        'PASSWORD':'TILoyAq5cE',
         'PORT':3307
     }
 }
